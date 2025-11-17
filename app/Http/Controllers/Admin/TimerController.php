4362311de1222f3c4043d76fb5d7877eef5c9718
DamianDell <damian@dell.local>
2025-11-17 14:09:10 +0100

    remove unused class

--- app/Http/Controllers/Admin/TimerController.php
+++ app/Http/Controllers/Admin/TimerController.php
@@ -2,25 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests\UserEditRequest;
-use App\Transformer\UserTransformerInterface;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\UserCreateRequest;
-use App\Models\User;
-use App\Repository\UserRepositoryInterface;
-use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class TimerController extends Controller
 {
-    public function index(UserRepositoryInterface $userRepo): Response
+    public function index(): Response
     {
-        $users = $userRepo->findByLimit(10);
-
-        return Inertia::render('Dashboard/Timer/Index', [
-            // 'users' => $users,
-        ]);
+        return Inertia::render('Dashboard/Timer/Index');
     }
 
 }
